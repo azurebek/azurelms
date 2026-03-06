7846958f4024ecd4617b0676b26f26f894f2be4e
azurebek <2510505504@ogrenci.karabuk.edu.tr>
2026-03-07 00:51:10 +0300

    Final production settings tweak for deployment

--- Dockerfile
+++ Dockerfile
@@ -18,5 +18,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 # 5. Kodni ko'chirish
 COPY . .
 
-# 6. Django-ni yoqish (App Platform buni o'zi boshqaradi, lekin bu yerda turishi zarar qilmaydi)
+# 6. Django static fayllarini yig'ish (Buni yurgizish uchun dummy o'zgaruvchilar)
+RUN SECRET_KEY=xxx USE_POSTGRES=False python manage.py collectstatic --noinput
+
+# 7. Django-ni yoqish (App Platform buni o'zi boshqaradi, lekin bu yerda turishi zarar qilmaydi)
 CMD ["gunicorn", "--bind", "0.0.0.0:8080", "core.wsgi:application"]
--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ TELEGRAM_WEBHOOK_SECRET = os.getenv('TELEGRAM_WEBHOOK_SECRET', 'YOUR_SECRET_TOKE
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -187,6 +188,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Logging configuration
 LOGGING = {
